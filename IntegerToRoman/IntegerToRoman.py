2f10f040b2773089b63d56850ba9dc34fb554926
schuyler <sasplin@ewu.edu>
2022-09-16 15:26:20 -0700

    perfect solution.

--- IntegerToRoman/IntegerToRoman.py
+++ IntegerToRoman/IntegerToRoman.py
@@ -1,136 +1,15 @@
-from math import floor
-from operator import mod
-
-
 class Solution:
     def intToRoman(self, num: int) -> str:
         
-        modNum = num
+        localNum = num
         ret = ""
-        behind = False
-        sequence = [1,5,10,50,100,500,1000]
-        seq = [1000,500,100,50,10,5,1]
+        val = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
+        numerals = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I']      
         
-        def convert (val: int) -> str:
-            if val == 1:
-                return 'I'
-            elif val == 5:
-                return 'V'
-            elif val == 10:
-                return 'X'
-            elif val == 50:
-                return 'L'
-            elif val == 100:
-                return 'C'
-            elif val == 500:
-                return 'D'
-            elif val == 1000:
-                return 'M'
+        for i in range(len(val)):
             
-        def numeralsShouldBeSwitched(modNum:int, seqPosition:int) -> bool:
-            
-            if ((modNum != seq[seqPosition]) and (seqPosition < len(seq)-1) and 
-                (modNum + nextPrecursor(seq[seqPosition]) >= seq[seqPosition])):
-                
-                print(f" YOOOO ({modNum}+{nextPrecursor(seq[seqPosition])} >= {seq[seqPosition]})")
-                return True
-            else: return False
-        
-        
-        def digitsAreSameLength(i:int, *seq) -> bool:
-           
-            # print(seq)
-            seq = seq[0]
-            # print (seq)
-
-            if (i >= len(seq)-2):
-                print("digitsAreSameLength if ")
-                return False
-            
-            elif (len(str(seq[i])) == len(str(seq[i+1]))):
-                print ("digitsAreSameLength elif: "+str(len(str(seq[i]))) +" "+ str(len(str(seq[i+1]))))
-                return True
-            else:
-                print ("digitsAreSameLength else: "+str(len(str(seq[i]))) +" "+ str(len(str(seq[i+1]))))
-                return False
-             
-        def findNextPrecursor(i:int, seq) -> int:
-            answer = 0
-            print("seq[i] in precursor",seq[i])
-            if i == (len(seq)-1):
-                print('findNext called on I')
-                return 0
-            else :
-                for x in range(i,len(seq)):
-                    ++answer
-                    if seq[x] % 10 == 0 or seq[x] == 1:
-                        return answer
-                    
-            return 0
-        
-        def nextPrecursor(seqNumber: int) -> int:
-            if seqNumber > 100:
-                return 100
-            elif seqNumber > 10:
-                return 10
-            else: return 1
-        
-        i = 0
-        for i in range(len(seq)):
-            
-            quotient = floor(modNum / seq[i]) 
-            
-            numeral = convert(seq[i])
-
-            if modNum < seq[i] and not numeralsShouldBeSwitched(modNum, i):
-                print('on:', seq[i],'do nothing. modNum', modNum)
-            if i == 0:
-                ret += numeral*int(quotient)
-                modNum %= 1000
-            elif modNum >= 50 and modNum < 90:
-                ret += 'L'
-                modNum %= 50
-            elif modNum >= 500 and modNum < 900:
-                ret += 'D'
-                modNum %= 500
-            # elif i == len(seq)-1:
-            #     print("on 1s")
-            else: 
-                if (i < len(seq)-1 and numeralsShouldBeSwitched(modNum, i)):
-                    
-                    precursorNum = nextPrecursor(seq[i])
-                    print ("precursorNum",precursorNum)
-                    ret += convert(precursorNum)+convert(seq[i])
-                    print("FOR if. modNum:",modNum," on: ",seq[i],)
-                    print("subtracting: ",seq[i+precursorNum] * (modNum / seq[i+precursorNum]))
-                    modNum -= precursorNum * (modNum / precursorNum)
-                    
-                    print("quotient",quotient)
-                    # i = i+1
-                    print("After ++i and modNum mod. on:",modNum," seqNumber: ",seq[i])
-                else:
-                    ret += numeral*int(quotient)
-                    print("FOR else. modNum:", modNum, " on: ", seq[i])
-                    modNum %= seq[i]
-                    print("After ++i and modNum mod modNum:", modNum, " seqNumber: ", seq[i])
-            
-            print("ret = ",ret)
-            print()
-            # if digitsAreSameLength(i, seq) and modNum >= seq[i] - seq[i+1]:
-            #     # i.e. modNum = 999. only 500 is subtracted instead of 900
-            #     print("FOR if. modNum:" + str(modNum) + " seqNumber: " + str(seq[i]))
-            #     i = i+1
-            #     modNum %= seq[i]
-            #     print("After ++i and modNum mod. modNum:" + str(modNum) + " seqNumber: " + str(seq[i]))
-            #     print()
-                
-            # else:
-            #     modNum %= seq[i]
-            #     print("FOR else. modNum: "+ str(modNum) + " seqNumber: " + str(seq[i]))
-            #     print()
-            # if (modNum == 0):
-            #     return ret
+            while (val[i] <= localNum):
+                ret += numerals[i]
+                localNum -= val[i]
 
         return(ret)
-        
-        
